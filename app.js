29ff3e82df58c4598d7f74c69de6cd1360c6c613
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-01-14 11:27:12 -0800

    View Employee function complete

--- app.js
+++ app.js
@@ -242,3 +242,20 @@ function viewRole() {
     });
 }
 
+// View Employee
+function viewEmployee() {
+    const query = `SELECT  employee.id,  employee.first_name, employee.last_name, role.title, role.salary, 
+    department.name, concat(manager.first_name, " ", manager.last_name) manager
+    FROM employee LEFT JOIN role ON role.id = employee.role_id
+    LEFT JOIN department ON department.id = role.department_id
+    LEFT JOIN employee manager ON manager.id = employee.manager_id`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        printTable(res);
+        startApp();
+    });
+}
+
+
+
