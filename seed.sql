0e705fab8778f805b85562705642ace003eb22ca
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-01-19 19:01:43 -0800

    Fixed schema

--- seed.sql
+++ seed.sql
@@ -8,26 +8,26 @@ USE employee_tracker;
 ---Create Department Table----
 
 CREATE TABLE department (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30)
 );
 
 ----Create Role Table----
 
 CREATE TABLE role (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
-title VARCHAR(30)
-salary DECIMAL
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL,
 department_id INT
 );
 
 ----Create Employee Table----
 
 CREATE TABLE employee (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
-first_name VARCHAR(30)
-last_name VARCHAR(30)
-role_id INT
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
 manager_id INT
 );
 
